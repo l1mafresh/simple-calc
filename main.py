ea233c1c67a685d23c4e62093ed661450cd497f6
l1mafresh <omdjeyy@gmail.com>
2024-07-20 12:48:41 +0300

    Engineering mode added

--- main.py
+++ main.py
@@ -11,14 +11,20 @@ class CalcApp(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.init_UI()
 
-    def init_UI(self):
-        
+    def init_UI(self):     
         buttons = {
             "btn_0": "0", "btn_1": "1", "btn_2": "2", "btn_3": "3",
             "btn_4": "4", "btn_5": "5", "btn_6": "6", "btn_7": "7",
-            "btn_8": "8", "btn_9": "9", "btn_leftbracket": "(",
-            "btn_rightbracket": ")", "btn_plus": "+", "btn_minus": "-",
-            "btn_multiple": "*", "btn_divide": "/", "btn_dot": "."
+            "btn_8": "8", "btn_9": "9", "btn_dot": ".",
+            "btn_plus": "+", "btn_minus": "-",
+            "btn_multiple": "*", "btn_divide": "/",
+            "btn_0_2": "0", "btn_1_2": "1", "btn_2_2": "2",
+            "btn_3_2": "3", "btn_4_2": "4", "btn_5_2": "5",
+            "btn_6_2": "6", "btn_7_2": "7", "btn_8_2": "8",
+            "btn_9_2": "9", "btn_dot_2": ".",
+            "btn_plus_2": "+", "btn_minus_2": "-",
+            "btn_multiple_2": "*", "btn_divide_2": "/",
+            "btn_leftbracket": "(", "btn_rightbracket": ")"
         }
 
         for btn_name, symbol in buttons.items():
@@ -30,12 +36,26 @@ class CalcApp(QtWidgets.QMainWindow):
         self.ui.btn_equal.clicked.connect(self.calculate)
         self.ui.btn_square.clicked.connect(self.square)
 
-        self.ui.act_info.triggered.connect(self.show_about_message)
+        self.ui.btn_log.clicked.connect(self.log)
+        self.ui.btn_sin.clicked.connect(self.sin)
+        self.ui.btn_cos.clicked.connect(self.cos)
+        self.ui.btn_tan.clicked.connect(self.tan)
+        self.ui.btn_exp.clicked.connect(self.exp)
+        self.ui.btn_pi.clicked.connect(self.pi)
+        self.ui.btn_e.clicked.connect(self.e)
+        #self.ui.btn_pow.clicked.connect(self.pow)
+        self.ui.btn_procent.clicked.connect(self.procent)
+
         self.ui.act_system.triggered.connect(self.system_theme)
         self.ui.act_light.triggered.connect(self.light_theme)
         self.ui.act_dark.triggered.connect(self.dark_theme)
         self.ui.act_on_trans.triggered.connect(self.on_transparent)
         self.ui.act_off_trans.triggered.connect(self.off_transparent)
+        
+        self.ui.actionStandard.triggered.connect(self.switch_to_standard)
+        self.ui.actionEngineer.triggered.connect(self.switch_to_engineer)
+        
+        self.ui.act_info.triggered.connect(self.show_about_message)
 
     # Enter numbers in a line with buttons
     def write_number(self, number):
@@ -54,7 +74,7 @@ class CalcApp(QtWidgets.QMainWindow):
             res = eval(self.ui.line_result.text())
             self.ui.line_result.setText(str(res))
         except:
-            QMessageBox.warning(self, "Error", "Incorrect expression")
+            self.error()
 
     # Clear result line
     def clear_line_result(self):
@@ -70,7 +90,7 @@ class CalcApp(QtWidgets.QMainWindow):
                 QMessageBox.warning(self, "Error",
                                     "It is not possible to extract the root from a negative value")
         except:
-            QMessageBox.warning(self, "Error", "Incorrect expression")
+            self.error()
 
     # Square
     def square(self):
@@ -78,7 +98,7 @@ class CalcApp(QtWidgets.QMainWindow):
             res = eval(self.ui.line_result.text())
             self.ui.line_result.setText(str(res*res))
         except:
-            QMessageBox.warning(self, "Error", "Incorrect expression")
+            self.error()
 
     # Delete last symbol
     def del_text(self):
@@ -86,12 +106,67 @@ class CalcApp(QtWidgets.QMainWindow):
         new_text = current_text[:-1]
         self.ui.line_result.setText(new_text)
 
-    # Show window with info about program
-    def show_about_message(self):
-        QMessageBox.information(self, "Simple calc",
-                                "Calc, writed \non Python and PyQt6.\n(c) limafresh")
+    # Engineer mode operations
+    def log(self):
+        try:
+            res = eval(self.ui.line_result.text())
+            self.ui.line_result.setText(str(log(res)))
+        except:
+            self.error()
+    def sin(self):
+        try:
+            res = eval(self.ui.line_result.text())
+            self.ui.line_result.setText(str(sin(res)))
+        except:
+            self.error()
+    def cos(self):
+        try:
+            res = eval(self.ui.line_result.text())
+            self.ui.line_result.setText(str(cos(res)))
+        except:
+            self.error()
+    def tan(self):
+        try:
+            res = eval(self.ui.line_result.text())
+            self.ui.line_result.setText(str(tan(res)))
+        except:
+            self.error()
+    def exp(self):
+        try:
+            res = eval(self.ui.line_result.text())
+            self.ui.line_result.setText(str(exp(res)))
+        except:
+            self.error()
+    def pi(self):
+        try:
+            if self.ui.line_result.text() == "" or self.ui.line_result.text() == "0":
+                self.ui.line_result.setText(str(pi))
+            else:
+                res = eval(self.ui.line_result.text())
+                self.ui.line_result.setText(str(res*pi))
+        except:
+            self.error()
+    def e(self):
+        try:
+            if self.ui.line_result.text() == "" or self.ui.line_result.text() == "0":
+                self.ui.line_result.setText(str(e))
+            else:
+                res = eval(self.ui.line_result.text())
+                self.ui.line_result.setText(str(res*e))
+        except:
+            self.error()
+    def procent(self):
+        try:
+            res = eval(self.ui.line_result.text())
+            self.ui.line_result.setText(str(res/100))
+        except:
+            self.error()
+            
+    # Error
+    def error(self):
+        QMessageBox.warning(self, "Error", "Incorrect expression")
 
-    # Change theme
+    # Change style
     def change_theme(self, bg_color, text_color):
         self.setStyleSheet(f"background-color: {bg_color}; color: {text_color};")
     def system_theme(self):
@@ -100,11 +175,21 @@ class CalcApp(QtWidgets.QMainWindow):
         self.change_theme("white", "black")
     def dark_theme(self):
         self.change_theme("black", "white")
-
     def on_transparent(self):
         self.setWindowOpacity(0.7)
     def off_transparent(self):
         self.setWindowOpacity(1)
+
+    # Change mode
+    def switch_to_standard(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.page)
+    def switch_to_engineer(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.page_2)
+
+    # Show window with info about program
+    def show_about_message(self):
+        QMessageBox.information(self, "Simple calc",
+                                "Calc, writed \non Python and PyQt6.\n(c) limafresh")
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
